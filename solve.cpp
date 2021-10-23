25de8bb3b66c5600bb5e6061cd94685e6477b0c9
bugwithbutts <nikitan-5@mail.ru>
2021-10-23 13:28:40 +0300

    geg

---
+++ solve.cpp
@@ -0,0 +1,24 @@
+#include "classes.h"
+using namespace std;
+
+void solve(vector<Stand>& Stands, vector<Plane>& Planes)
+{
+	vector<Plane> was(Stands.size(), 0);
+	vector<int> ans(Planes.size());
+	for(auto plane: Planes)
+	{
+		int mn = 1000000000;
+		int poz = 0;
+		for(auto stand: Stands)
+		{
+			if(was[stand.Aircraft_Stand] < plane && cost(plane, stand) < mn)
+			{
+				mn = cost(plane, stand);
+				poz = stand.Aircraft_Stand;				
+			}
+		}
+		was[poz] = plane;
+		ans[plane.id] = poz;
+	}
+	return ans;		
+}
